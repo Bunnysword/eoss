0bcb9f2b07e91def2280f71959b48fc89ee5d523
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-02 01:03:09 +0300

    Update eoss.sh

--- eoss.sh
+++ eoss.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo pacman -Sy openrgb fastfetch inxi discord steam telegram-desktop transmission-qt nvtop btop code mangohud lib32-mangohud ffmpeg obs-studio flatpak partitionmanager libva-nvidia-driver
+sudo pacman -S nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings lib32-opencl-nvidia opencl-nvidia libxnvctrl lib32-vulkan-icd-loader libva-nvidia-driver
 yay -S coolercontrol-bin
 flatpak install flathub -y net.davidotek.pupgui2
 sudo systemctl enable --now coolercontrold
