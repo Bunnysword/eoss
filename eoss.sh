817228c3686e2a029bd4a68ea57be8db1abaceda
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-01 16:39:29 +0300

    Create eoss.sh

---
+++ eoss.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo pacman -Sy openrgb fastfetch inxi discord steam telegram-desktop transmission-qt nvtop btop code mangohud ffmpeg obs-studio flatpak
+yay -S coolercontrol-bin
+flatpak install flathub -y net.davidotek.pupgui2
+sudo systemctl enable --now coolercontrold
+wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/configurations.sh
+chmod 750 configurations.sh && ./configurations.sh
