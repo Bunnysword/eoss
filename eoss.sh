bf9b1269f51df2cf651607405c6be0e0a7d5a490
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-09 18:36:49 +0300

    Update eoss.sh

--- eoss.sh
+++ eoss.sh
@@ -7,3 +7,5 @@ flatpak install flathub -y net.davidotek.pupgui2
 sudo systemctl enable --now coolercontrold
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/configurations.sh
 chmod 750 configurations.sh && ./configurations.sh
+git clone https://github.com/Bunnysword/KDE-Sword-theme/
+cd KDE-Sword-theme/ && chmod 750 kdeswordtheme.sh && ./kdeswordtheme.sh
