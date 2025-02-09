5253a6f85de9e996c5b699f2ea87a709834be738
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-09 18:37:24 +0300

    Update eoss.sh

--- eoss.sh
+++ eoss.sh
@@ -2,9 +2,8 @@
 
 sudo pacman -Sy openrgb fastfetch inxi discord steam telegram-desktop transmission-qt nvtop btop code mangohud lib32-mangohud ffmpeg obs-studio flatpak partitionmanager fish papirus-icon-theme
 sudo pacman -S nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings lib32-opencl-nvidia opencl-nvidia libxnvctrl lib32-vulkan-icd-loader libva-nvidia-driver
-yay -S coolercontrol-bin
+yay -S coolercontrol-bin && sudo systemctl enable --now coolercontrold
 flatpak install flathub -y net.davidotek.pupgui2
-sudo systemctl enable --now coolercontrold
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/configurations.sh
 chmod 750 configurations.sh && ./configurations.sh
 git clone https://github.com/Bunnysword/KDE-Sword-theme/
