ea4bfe9a780b4dd7fb2eb1cc00ae33b15f920106
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-02-09 18:32:53 +0300

    Update eoss.sh

--- eoss.sh
+++ eoss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Sy openrgb fastfetch inxi discord steam telegram-desktop transmission-qt nvtop btop code mangohud lib32-mangohud ffmpeg obs-studio flatpak partitionmanager fish
+sudo pacman -Sy openrgb fastfetch inxi discord steam telegram-desktop transmission-qt nvtop btop code mangohud lib32-mangohud ffmpeg obs-studio flatpak partitionmanager fish papirus-icon-theme
 sudo pacman -S nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings lib32-opencl-nvidia opencl-nvidia libxnvctrl lib32-vulkan-icd-loader libva-nvidia-driver
 yay -S coolercontrol-bin
 flatpak install flathub -y net.davidotek.pupgui2
