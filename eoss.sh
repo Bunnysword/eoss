344c3df68c7198e9da147b25f58d56fdd8e78e0d
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-07-26 13:49:49 +0300

    Update eoss.sh

--- eoss.sh
+++ eoss.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 #pacman
-sudo pacman -Sy fastfetch inxi discord steam telegram-desktop transmission-qt nvtop btop code mangohud lib32-mangohud ffmpeg obs-studio flatpak partitionmanager fish papirus-icon-theme imagemagick htop cassette
+sudo pacman -Sy fastfetch inxi steam telegram-desktop transmission-qt nvtop btop code mangohud lib32-mangohud ffmpeg obs-studio flatpak partitionmanager fish papirus-icon-theme imagemagick htop gpu-screen-recorder-ui
 #nvidia
 sudo pacman -S nvidia nvidia-utils lib32-nvidia-utils nvidia-settings lib32-opencl-nvidia opencl-nvidia libxnvctrl lib32-vulkan-icd-loader libva-nvidia-driver
-sudo systemctl restart bluetooth
 
 #aur
 yay -S coolercontrol-bin protonup-qt && sudo systemctl enable --now coolercontrold
@@ -15,6 +14,7 @@ flatpak install flathub -y net.davidotek.pupgui2
 wget https://raw.githubusercontent.com/Bunnysword/Confiles/refs/heads/main/configurations.sh
 chmod 750 configurations.sh && ./configurations.sh
 
+systemctl enable --now --user gpu-screen-recorder-ui
 git clone https://github.com/Bunnysword/KDE-Sword-Configuration.git
 cd KDE-Sword-Configuration/ && chmod 750 kdeswordtheme.sh && ./kdeswordtheme.sh
 #waybar hyprshot nftables hyprpolkitagent ntfs-3g
