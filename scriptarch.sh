cf5370e78e60d08907cafd08eb4f3aa820842257
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-10-26 00:47:28 +0300

    Update scriptarch.sh

--- scriptarch.sh
+++ scriptarch.sh
@@ -15,7 +15,7 @@ sudo locale-gen && localectl set-locale LANG=ru_RU.UTF-8
 
 yay -Syu coolercontrol-bin protonup-qt kdeconnect dosfstools \
 fastfetch inxi nvtop btop htop ffmpeg fish ntfs-3g flatpak mpv alsa-utils nftables \
-steam transmission-qt code mangohud lib32-mangohud partitionmanager \
+steam transmission-qt code mangohud lib32-mangohud partitionmanager libva \
 papirus-icon-theme imagemagick noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gpu-screen-recorder-ui 
 
 #nvidia
