41bcfd039f054e213fb825d6a18e616266398521
Bunnysword <129748119+Bunnysword@users.noreply.github.com>
2025-10-26 05:44:07 +0300

    Update scriptarch.sh

--- scriptarch.sh
+++ scriptarch.sh
@@ -13,7 +13,7 @@ sudo sed -i s/'#en_US.UTF-8'/'en_US.UTF-8'/g /etc/locale.gen
 sudo sed -i s/'#ru_RU.UTF-8'/'ru_RU.UTF-8'/g /etc/locale.gen
 sudo locale-gen && localectl set-locale LANG=ru_RU.UTF-8
 
-yay -Syu coolercontrol-bin protonup-qt kdeconnect dosfstools \
+yay -Syu coolercontrol-bin protonup-qt kdeconnect dosfstools rar \
 fastfetch inxi nvtop btop htop ffmpeg fish ntfs-3g flatpak mpv alsa-utils nftables \
 steam transmission-qt code mangohud lib32-mangohud partitionmanager libva \
 papirus-icon-theme imagemagick noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra gpu-screen-recorder-ui 
